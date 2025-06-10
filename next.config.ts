24fba24b0586e55e75c8aa44b1f513b9f2bde0d9
Midoryan <ryan.adisanjaya@gmail.com>
2025-06-11 04:29:49 +0700

    chore(config): minor updates to next.config.js and tsconfig.json

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,17 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'images.unsplash.com', 
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '', 
+      },
+      {
+        protocol: 'https',
         hostname: 'via.placeholder.com',
         port: '',
         pathname: '/**', 
